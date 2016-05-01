e2e5d087771e8bd0c03b7e8b8c14ea897b43947a
maheshlnties <maheshlnties@gmail.com>
2016-05-01 16:27:26 +0530

    modified

--- SportsComplex.Database/SqlQueries.cs
+++ SportsComplex.Database/SqlQueries.cs
@@ -97,45 +97,45 @@ END
 
         public const string SqlSelectResourceSettings = "SELECT * FROM [SportsComplex].[dbo].[ResourceSettings]";
 
-        public const string SqlSelectBadmintonResource = "SELECT * FROM [SportsComplex].[dbo].[BadmintonResource] WHERE BookDate = '{0}'";
+        //public const string SqlSelectBadmintonResource = "SELECT * FROM [SportsComplex].[dbo].[BadmintonResource] WHERE BookDate = '{0}'";
 
-        //public const string SqlSelectBadmintonResource = "SELECT * FROM [SportsComplex].[dbo].[BadmintonResource] WHERE BookDate = convert(date , '{0}' , 105)";
+        public const string SqlSelectBadmintonResource = "SELECT * FROM [SportsComplex].[dbo].[BadmintonResource] WHERE BookDate = convert(date , '{0}' , 105)";
 
-        public const string SqlSelectBilliardResource = "SELECT * FROM [SportsComplex].[dbo].[BilliardResource] WHERE BookDate = '{0}'";
+        //public const string SqlSelectBilliardResource = "SELECT * FROM [SportsComplex].[dbo].[BilliardResource] WHERE BookDate = '{0}'";
 
-        //public const string SqlSelectBilliardResource = "SELECT * FROM [SportsComplex].[dbo].[BilliardResource] WHERE BookDate = convert(date , '{0}' , 105)";
+        public const string SqlSelectBilliardResource = "SELECT * FROM [SportsComplex].[dbo].[BilliardResource] WHERE BookDate = convert(date , '{0}' , 105)";
 
-        public const string SqlAddImage = "INSERT INTO [SportsComplex].[dbo].[Gallery] ([Name],[EncodedImage],[UploadedOn]) VALUES('{0}','{1}','{2}')";
+        //public const string SqlAddImage = "INSERT INTO [SportsComplex].[dbo].[Gallery] ([Name],[EncodedImage],[UploadedOn]) VALUES('{0}','{1}','{2}')";
 
-        //public const string SqlAddImage = "INSERT INTO [SportsComplex].[dbo].[Gallery] ([Name],[EncodedImage],[UploadedOn]) VALUES('{0}','{1}',convert(date , '{2}' , 105))";
+        public const string SqlAddImage = "INSERT INTO [SportsComplex].[dbo].[Gallery] ([Name],[EncodedImage],[UploadedOn]) VALUES('{0}','{1}',convert(date , '{2}' , 105))";
 
         public const string SqlDeleteImages = "DELETE FROM [SportsComplex].[dbo].[Gallery] Where Id = '{0}'";
 
         public const string SqlSelectImages = "SELECT * FROM [SportsComplex].[dbo].[Gallery]";
 
-        public const string SqlAddNews = "INSERT INTO [SportsComplex].[dbo].[News] ([Content],[Highlight],[PostedOn],[ExpiresOn]) VALUES('{0}','{1}','{2}','{3}')";
+        //public const string SqlAddNews = "INSERT INTO [SportsComplex].[dbo].[News] ([Content],[Highlight],[PostedOn],[ExpiresOn]) VALUES('{0}','{1}','{2}','{3}')";
 
-        //public const string SqlAddNews = "INSERT INTO [SportsComplex].[dbo].[News] ([Content],[Highlight],[PostedOn],[ExpiresOn]) VALUES('{0}','{1}',convert(date , '{2}' , 105),convert(date , '{3}' , 105))";
+        public const string SqlAddNews = "INSERT INTO [SportsComplex].[dbo].[News] ([Content],[Highlight],[PostedOn],[ExpiresOn]) VALUES('{0}','{1}',convert(date , '{2}' , 105),convert(date , '{3}' , 105))";
 
         public const string SqlDeleteNews = "DELETE FROM [SportsComplex].[dbo].[News] Where Id = '{0}'";
 
         public const string SqlSelectNews = "SELECT * FROM [SportsComplex].[dbo].[News]";
 
-        //public const string SqlAddGym = "INSERT INTO [SportsComplex].[dbo].[Gym] ([PsNumber],[TransactionDate],[Joined],[JoinedOn],[LeftOn]) VALUES('{0}',convert(date , '{1}' , 105),'{2}',convert(date , '{3}' , 105),convert(date , '{4}' , 105))";
+        public const string SqlAddGym = "INSERT INTO [SportsComplex].[dbo].[Gym] ([PsNumber],[TransactionDate],[Joined],[JoinedOn],[LeftOn]) VALUES('{0}',convert(date , '{1}' , 105),'{2}',convert(date , '{3}' , 105),convert(date , '{4}' , 105))";
 
-        public const string SqlAddGym = "INSERT INTO [SportsComplex].[dbo].[Gym] ([PsNumber],[TransactionDate],[Joined],[JoinedOn],[LeftOn]) VALUES('{0}','{1}','{2}','{3}','{4}')";
+        //public const string SqlAddGym = "INSERT INTO [SportsComplex].[dbo].[Gym] ([PsNumber],[TransactionDate],[Joined],[JoinedOn],[LeftOn]) VALUES('{0}','{1}','{2}','{3}','{4}')";
 
-        //public const string SqlUpdateGym = "UPDATE [SportsComplex].[dbo].[Gym] SET [TransactionDate] = convert(date , '{0}' , 105),[Joined] = '{1}',[LeftOn] = convert(date , '{2}' , 105) WHERE Id='{3}'";
+        public const string SqlUpdateGym = "UPDATE [SportsComplex].[dbo].[Gym] SET [TransactionDate] = convert(date , '{0}' , 105),[Joined] = '{1}',[LeftOn] = convert(date , '{2}' , 105) WHERE Id='{3}'";
 
-        public const string SqlUpdateGym = "UPDATE [SportsComplex].[dbo].[Gym] SET [TransactionDate] = '{0}',[Joined] = '{1}',[LeftOn] = '{2}' WHERE Id='{3}'";
+        //public const string SqlUpdateGym = "UPDATE [SportsComplex].[dbo].[Gym] SET [TransactionDate] = '{0}',[Joined] = '{1}',[LeftOn] = '{2}' WHERE Id='{3}'";
 
         public const string SqlSelectGymByPsNumber = "SELECT * FROM [SportsComplex].[dbo].[Gym] Where Joined = 1 AND PsNumber='{0}'";
 
         public const string SqlSelectGmyById = "SELECT * FROM [SportsComplex].[dbo].[Gym] Where Id ='{0}'";
 
-        //public const string SqlAddTournment = "INSERT INTO [SportsComplex].[dbo].[Tournment] ([Name],[Fees],[CreatedDate],[LastDate],[IsDeleted]) VALUES('{0}','{1}',convert(date , '{2}' , 105),convert(date , '{3}' , 105),'{4}')";
+        public const string SqlAddTournment = "INSERT INTO [SportsComplex].[dbo].[Tournment] ([Name],[Fees],[CreatedDate],[LastDate],[IsDeleted]) VALUES('{0}','{1}',convert(date , '{2}' , 105),convert(date , '{3}' , 105),'{4}')";
 
-        public const string SqlAddTournment = "INSERT INTO [SportsComplex].[dbo].[Tournment] ([Name],[Fees],[CreatedDate],[LastDate],[IsDeleted]) VALUES('{0}','{1}','{2}','{3}','{4}')";
+        //public const string SqlAddTournment = "INSERT INTO [SportsComplex].[dbo].[Tournment] ([Name],[Fees],[CreatedDate],[LastDate],[IsDeleted]) VALUES('{0}','{1}','{2}','{3}','{4}')";
 
         public const string SqlDeleteTournment = "DELETE FROM [SportsComplex].[dbo].[Tournment] Where Id = '{0}'";
 
@@ -143,9 +143,9 @@ END
 
         public const string SqlSelectTournment = "SELECT * FROM [SportsComplex].[dbo].[Tournment]";
 
-        public const string SqlBookTournment = "INSERT INTO [SportsComplex].[dbo].[TournmentBooking] ([TournmentId],[PsNumber],[TransactionDate]) VALUES('{0}','{1}','{2}')";
+        //public const string SqlBookTournment = "INSERT INTO [SportsComplex].[dbo].[TournmentBooking] ([TournmentId],[PsNumber],[TransactionDate]) VALUES('{0}','{1}','{2}')";
 
-        //public const string SqlBookTournment = "INSERT INTO [SportsComplex].[dbo].[TournmentBooking] ([TournmentId],[PsNumber],[TransactionDate]) VALUES('{0}','{1}','convert(date , '{2}' , 105)')";
+        public const string SqlBookTournment = "INSERT INTO [SportsComplex].[dbo].[TournmentBooking] ([TournmentId],[PsNumber],[TransactionDate]) VALUES('{0}','{1}','convert(date , '{2}' , 105)')";
 
         public const string SqlSelectTournmentBookingByPsNumber = "SELECT * FROM [SportsComplex].[dbo].[TournmentBooking] Where PsNumber='{0}'";
 
